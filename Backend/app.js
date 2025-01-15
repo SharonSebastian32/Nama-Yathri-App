279ff902d42eecf98921c05f24eb9dae858d90aa
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2025-01-15 09:25:29 +0530

    user authentication completed

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const express = require("express");
@@ -10,6 +11,7 @@ const app = express();
 connectToDb();
 app.use(morgan("dev"));
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
