30754d4f073732e13b7de4d5b65f661c6257c92e
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2025-01-15 11:00:38 +0530

    user model created

---
+++ Backend/models/captain.model.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const captainSchema = new mongoose.Schema(
+  {
+    fullname: {
+      firstname: {
+        type: String,
+        required: [true, "Firstname is required"],
+        minlength: [3, "Firstname must be at least 3 characters long"],
+      },
+      lastname: {
+        type: String,
+        minlength: [3, "Lastname must be at least 3 characters long"],
+      },
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+    socketId: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "inactive",
+    },
+    vehicle: {
+      color: {
+        type: String,
+        required: [true, "Color is required"],
+        minlength: [3, "Color must be at least 3 characters long"],
+      },
+      plate: {
+        type: String,
+        required: [true, "Plate is required"],
+        minlength: [3, "Plate must be at least 3 characters long"],
+      },
+      capacity: {
+        type: Number,
+        required: [true, "Capacity is required"],
+        min: [1, "Capacity must be at least 1"],
+      },
+      vehicleType: {
+        type: String,
+        required: [true, "Vehicle type is required"],
+        enum: ["car", "motorcycle", "auto"],
+      },
+    },
+    location: {
+      ltd: {
+        type: Number,
+      },
+      lng: {
+        type: Number,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+captainSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET. {expiresIn: '24h'});
+};
+
+const captainModel = mongoose.model("captain", captainSchema);
+module.exports = captainModel;
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -2,33 +2,38 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
-  fullname: {
-    firstname: {
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      firstname: {
+        type: String,
+        required: true,
+        minlength: [3, "First name must be at least 3 characters long"],
+      },
+      lastname: {
+        type: String,
+        minlength: [3, "Last name must be at least 3 characters long"],
+      },
+    },
+    email: {
       type: String,
       required: true,
-      minlength: [3, "First name must be at least 3 characters long"],
+      unique: true,
+      minlength: [5, "Email must be at least 5 characters long"],
     },
-    lastname: {
+    password: {
       type: String,
-      minlength: [3, "Last name must be at least 3 characters long"],
+      required: true,
+      select: false,
+    },
+    socketId: {
+      type: String,
     },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: [5, "Email must be at least 5 characters long"],
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  socketId: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
